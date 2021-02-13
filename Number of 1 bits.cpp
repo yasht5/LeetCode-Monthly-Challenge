34b7846fa577981678ac9fb8d558b4b480474214
Yash Toshniwal <65651244+yasht5@users.noreply.github.com>
2021-02-13 19:01:06 +0530

    Create Number of 1 bits.cpp

---
+++ Number of 1 bits.cpp
@@ -0,0 +1,27 @@
+/*
+
+  Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+  Note:
+
+  Note that in some languages such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+  In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+
+*/
+
+class Solution{
+	
+	public:
+		int hammingWeight(uint32_t n)
+		{
+			int ans=0;
+			uint32_t mask = 1;
+			for(int i=0;i<32;i++)
+			{
+				if((n & mask)!=0)
+					ans++;
+				mask = mask<<1;
+			}
+			return ans;
+		}
+};
