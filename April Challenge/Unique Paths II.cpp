d1a62965ae45e1d338c8c49136eca0856fa0a233
Yash Toshniwal <65651244+yasht5@users.noreply.github.com>
2021-04-28 17:25:17 +0530

    Create Unique Paths II.cpp

---
+++ April Challenge/Unique Paths II.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& A) {
+        int n = A.size();
+        int m = A[0].size();
+        
+        vector<vector<int>> dp(n, vector<int> (m, 0));
+        if(A[0][0]==0)
+        {
+            dp[0][0]=1;
+        }
+        for(int i=1;i<n;i++)
+        {
+            if(A[i][0]==0)
+            {
+                dp[i][0] = dp[i-1][0];
+            }
+        }
+        for(int i=1;i<m;i++)
+        {
+            if(A[0][i]==0)
+            {
+                dp[0][i] = dp[0][i-1];
+            }
+        }
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<m;j++)
+            {
+                if(A[i][j]==0)
+                {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
