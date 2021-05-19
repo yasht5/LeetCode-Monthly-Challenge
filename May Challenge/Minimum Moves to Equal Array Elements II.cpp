54744ab779bf4502c8e04746e104a7d133294986
Yash Toshniwal <65651244+yasht5@users.noreply.github.com>
2021-05-19 14:38:10 +0530

    Create Minimum Moves to Equal Array Elements II.cpp

---
+++ May Challenge/Minimum Moves to Equal Array Elements II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int res = 0;
+        int i=0, j= nums.size()-1;
+        while(i<j)
+        {
+            res+=nums[j]-nums[i];
+            j--;
+            i++;
+        }
+        return res;
+    }
+};
