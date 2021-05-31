a99590a51c457762e65b9fd36fa7094226f94b03
Yash Toshniwal <65651244+yasht5@users.noreply.github.com>
2021-05-31 15:08:47 +0530

    Create Search Suggestions system.cpp

---
+++ May Challenge/Search Suggestions system.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        auto it = products.begin();
+        sort(it, products.end());
+        
+        vector<vector<string>> res;
+        string curr = "";
+        
+        for(char c : searchWord)
+        {
+            curr+=c;
+            vector<string> sugg;
+            it = lower_bound(it, products.end(), curr);
+            for(int i=0;i<3 && it+i!=products.end();i++)
+            {
+                string& s = *(it+i);
+                if(s.find(curr))
+                {
+                    break;
+                }
+                sugg.push_back(s);
+            }
+            res.push_back(sugg);
+        }
+        return res;
+    }
+};
