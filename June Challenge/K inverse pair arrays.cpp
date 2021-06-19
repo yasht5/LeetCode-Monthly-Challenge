66b1209eec880244a98167f48a0298f2ca0d6a7f
Yash Toshniwal <65651244+yasht5@users.noreply.github.com>
2021-06-19 15:53:07 +0530

    Create K inverse pair arrays.cpp

---
+++ June Challenge/K inverse pair arrays.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    
+    const int mod = 1000000007;
+    
+    int kInversePairs(int n, int k) {
+        int dp[2][1001] = { 1 };
+        for (int N = 1; N <= n; ++N)
+        {
+            for (int K = 0; K <= k; ++K) 
+            {
+               dp[N % 2][K] = (dp[(N - 1) % 2][K] + (K > 0 ? dp[N % 2][K - 1] : 0)) % mod;
+                if (K >= N)
+                {
+                    dp[N % 2][K] = (mod + dp[N % 2][K] - dp[(N - 1) % 2][K - N]) % mod;
+                }
+            }
+        }
+        return dp[n % 2][k];
+    }
+};
