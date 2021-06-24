0d5980584358a1e74c36f88d0be5a9a30dbe6da4
Yash Toshniwal <65651244+yasht5@users.noreply.github.com>
2021-06-24 13:46:20 +0530

    Create Out of Boundary paths.cpp

---
+++ June Challenge/Out of Boundary paths.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    const int mod = 1000000007;
+    
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[startRow][startColumn] = 1;
+        
+        int count = 0;
+        
+        for(int k = 1; k<=maxMove;k++)
+        {
+            vector<vector<int>> temp(m, vector<int>(n, 0));
+            for(int i=0;i<m;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(i==m-1)
+                    {
+                        count = (count + dp[i][j])%mod;
+                    }
+                    if(j==n-1)
+                    {
+                        count = (count + dp[i][j])%mod;
+                    }
+                    if(i==0)
+                    {
+                        count = (count + dp[i][j])%mod;
+                    }
+                    if(j==0)
+                    {
+                        count = (count + dp[i][j])%mod;
+                    }
+                    temp[i][j] = (((i>0 ? dp[i-1][j] : 0) + (i<m-1 ? dp[i+1][j] : 0))%mod + ((j>0 ? dp[i][j-1] : 0) + (j<n-1 ? dp[i][j+1] : 0))%mod)%mod;
+                }
+            }
+            
+            dp = temp;
+        }
+        return count;
+    }
+};
